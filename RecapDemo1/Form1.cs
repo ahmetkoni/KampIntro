e4501cea541cd2b473b93bf26cd2e54d0780a4d6
ahmetkoni <ahmetkoni7@gmail.com>
2022-08-12 15:50:45 +0300

    12 Agustos Dama

---
+++ RecapDemo1/Form1.cs
@@ -0,0 +1,39 @@
+namespace RecapDemo1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Button[,] buttons = new Button[8, 8];
+            int top = 0;
+            int left = 0;
+            for (int i = 0; i < buttons.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j < buttons.GetUpperBound(1); j++)
+                {
+                    buttons[i, j] = new Button();
+                    buttons[i, j].Width = 50;
+                    buttons[i, j].Height = 50;
+                    buttons[i, j].Left = left;
+                    buttons[i, j].Top = top;
+                    left += 50;
+                    if ((i + j) % 2 == 0)
+                    {
+                        buttons[i, j].BackColor = Color.Purple;
+                    }
+                    else
+                        buttons[i, j].BackColor = Color.Orange;
+                    this.Controls.Add(buttons[i, j]);
+                }
+                left = 0;
+                top += 50;
+            }
+            top = 0;
+        }
+    }
+}
